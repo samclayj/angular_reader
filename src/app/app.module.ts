80d2d225618e0843bb975377017ac792f28abbbc
Samuel Jentsch <sjentsch@Samuels-MBP.attlocal.net>
2020-03-12 06:45:10 -0500

    Added angular cards.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/**
+ * Angular Material imports.
+ */
+import { MatCardModule } from '@angular/material/card';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +18,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // Angular Material
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
